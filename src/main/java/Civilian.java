995c5d51ea94f491e702f18d0abc0f47b9b0ea9a
Cameron Jensen <jensenseanc@gmail.com>
2017-04-07 11:33:18 -0700

    add find, save, all, equals methods to civilian class

--- src/main/java/Civilian.java
+++ src/main/java/Civilian.java
@@ -8,6 +8,7 @@ public class Civilian extends Viewers implements Person {
     this.ranger = ranger;
     this.name = name;
     this.phone = phone;
+    this.id = id;
   }
 
   public boolean getRanger() {
@@ -20,5 +21,59 @@ public class Civilian extends Viewers implements Person {
 
   public String getPhone() {
     return phone;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  @Override
+  public boolean equals(Object otherCivilian) {
+    if(!(otherCivilian instanceof Civilian)) {
+      return false;
+    } else {
+      Civilian newCivilian = (Civilian) otherCivilian;
+      return this.getName().equals(newCivilian.getName()) &&
+             this.getRanger() == newCivilian.getRanger() && this.getPhone().equals(newCivilian.getPhone());
+    }
+  }
+
+  public static List<Civilian> all() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM viewers WHERE ranger = false;";
+      return con.createQuery(sql)
+        .executeAndFetch(Civilian.class);
+    }
+  }
+
+  public void save() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "INSERT INTO viewers (ranger, name, phone) VALUES (:ranger, :name, :phone);";
+      this.id = (int) con.createQuery(sql, true)
+        .addParameter("ranger", ranger)
+        .addParameter("name", name)
+        .addParameter("phone", phone)
+        .executeUpdate()
+        .getKey();
+    }
+  }
+
+  public void delete() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "DELETE FROM viewers WHERE id=:id;";
+      con.createQuery(sql)
+        .addParameter("id", id)
+        .executeUpdate();
+    }
+  }
+
+  public static Civilian find(int id) {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM viewers WHERE id=:id;";
+      Civilian civilian = con.createQuery(sql)
+        .addParameter("id", id)
+        .executeAndFetchFirst(Civilian.class);
+      return civilian;
+    }
   }
 }
--- src/test/java/CivilianTest.java
+++ src/test/java/CivilianTest.java
@@ -10,26 +10,68 @@ public class CivilianTest {
   public DatabaseRule database = new DatabaseRule();
 
   @Test
-  public void civilian_instantiatesCorrectly_true() {
-    Civilian testcivilian = new Civilian(true, "Randy", "802-234-9873");
-    assertEquals(true, testcivilian instanceof Civilian);
+  public void civilian_instantiatesCorrectly() {
+    Civilian testCivilian = new Civilian(false, "Randy", "802-234-9873");
+    assertTrue(testCivilian instanceof Civilian);
   }
 
   @Test
   public void getRanger_instantiatesCorrectly_boolean() {
-    Civilian testcivilian = new Civilian(true, "Randy", "802-234-9873");
-    assertEquals(true, testcivilian.getRanger());
+    Civilian testcivilian = new Civilian(false, "Randy", "802-234-9873");
+    assertEquals(false, testcivilian.getRanger());
   }
 
   @Test
   public void getName_instantiatesCorrectly_String() {
-    Civilian testcivilian = new Civilian(true, "Randy", "802-234-9873");
+    Civilian testcivilian = new Civilian(false, "Randy", "802-234-9873");
     assertEquals("Randy", testcivilian.getName());
   }
 
   @Test
   public void getPhone_instantiatesCorrectly_String() {
-    Civilian testcivilian = new Civilian(true, "Randy", "802-234-9873");
+    Civilian testcivilian = new Civilian(false, "Randy", "802-234-9873");
     assertEquals("802-234-9873", testcivilian.getPhone());
+  }
+
+  @Test
+  public void all_returnsAllInstancesOfCivilian_true() {
+    Civilian testCivilian = new Civilian(false, "Randy", "802-234-9873");
+    testCivilian.save();
+    Civilian otherCivilian = new Civilian(false, "Randy", "802-234-9873");
+    otherCivilian.save();
+    assertTrue(Civilian.all().get(0).equals(testCivilian));
+    assertTrue(Civilian.all().get(1).equals(otherCivilian));
+  }
+
+  @Test
+  public void equals_returnsTrueIfNameIsTheSame_true() {
+    Civilian testcivilian = new Civilian(false, "Randy", "802-234-9873");
+    Civilian othercivilian = new Civilian(false, "Randy", "802-234-9873");
+    assertTrue(testcivilian.equals(othercivilian));
+  }
+
+  @Test
+  public void save_assignsIdToObjectAndSavesObjectToDatabase() {
+    Civilian testCivilian = new Civilian(false, "Randy", "802-234-9873");
+    testCivilian.save();
+    Civilian savedCivilian = Civilian.all().get(0);
+    assertEquals(testCivilian.getId(), savedCivilian.getId());
+  }
+
+  @Test
+  public void delete_deletesCivilianFromDatabase_0() {
+    Civilian testCivilian = new Civilian(false, "Randy", "802-234-9873");
+    testCivilian.save();
+    testCivilian.delete();
+    assertEquals(0, Civilian.all().size());
+  }
+
+  @Test
+  public void find_returnsCivilianWithSameId_secondCivilian() {
+    Civilian testCivilian = new Civilian(false, "Randy", "802-234-9873");
+    testCivilian.save();
+    Civilian otherCivilian = new Civilian(false, "Randy", "802-234-9873");
+    otherCivilian.save();
+    assertEquals(otherCivilian, Civilian.find(otherCivilian.getId()));
   }
 }
